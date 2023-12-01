cf13e1cb496d78b1eecc1d6604c0af5cedbc4a28
Shaila Man <silky098@users.noreply.github.com>
2023-12-01 15:01:14 +1100

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  var args = process.argv;
+  if (args[2] && args[2] === "--v") {
+    console.log("Es6CliSkeleton - version 1.0.0");
+  } else {
+    console.log("Hello Advent of Code");
+  }
+}
+main();
