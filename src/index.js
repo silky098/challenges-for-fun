8f6d71c62cf62e5a23c509d5369fb51d81b64f5a
Shaila Man <silky098@users.noreply.github.com>
2023-12-02 16:34:06 +1100

    part b

--- src/index.js
+++ src/index.js
@@ -1,41 +1,68 @@
-//part a
-
 import games from "./games.js";
 
-function idTotals(games, allCubes) {
-  let sum = 0;
+//part a
+
+// function idTotals(games, allCubes) {
+//   let sum = 0;
+
+//   games.forEach((game) => {
+//     const gameParts = game.split(": ");
+//     const gameID = parseInt(gameParts[0].split(" ")[1]);
+//     const sets = gameParts[1].split("; ");
+
+//     let isPossible = true;
+//     for (const set of sets) {
+//       const cubeCounts = { red: 0, green: 0, blue: 0 };
+//       set.split(", ").forEach((cube) => {
+//         const [count, colour] = cube.split(" ");
+//         cubeCounts[colour] += parseInt(count);
+//       });
+
+//       if (
+//         cubeCounts.red > allCubes.red ||
+//         cubeCounts.green > allCubes.green ||
+//         cubeCounts.blue > allCubes.blue
+//       ) {
+//         isPossible = false;
+//         break;
+//       }
+//     }
+
+//     if (isPossible) {
+//       sum += gameID;
+//     }
+//   });
+
+//   return sum;
+// }
+
+// const allCubes = { red: 12, green: 13, blue: 14 };
+// const result = idTotals(games, allCubes);
+// console.log(result);
+
+// ----------------------------
+//part b
+
+function calcMinCubesAndTotalPower(games) {
+  let totalPowr = 0;
 
   games.forEach((game) => {
-    const gameParts = game.split(": ");
-    const gameID = parseInt(gameParts[0].split(" ")[1]);
-    const sets = gameParts[1].split("; ");
+    const sets = game.split(": ")[1].split("; ");
+    let maxCubes = { red: 0, green: 0, blue: 0 };
 
-    let isPossible = true;
-    for (const set of sets) {
-      const cubeCounts = { red: 0, green: 0, blue: 0 };
+    sets.forEach((set) => {
       set.split(", ").forEach((cube) => {
-        const [count, color] = cube.split(" ");
-        cubeCounts[color] += parseInt(count);
+        const [count, colour] = cube.split(" ");
+        maxCubes[colour] = Math.max(maxCubes[colour], parseInt(count));
       });
+    });
 
-      if (
-        cubeCounts.red > allCubes.red ||
-        cubeCounts.green > allCubes.green ||
-        cubeCounts.blue > allCubes.blue
-      ) {
-        isPossible = false;
-        break;
-      }
-    }
-
-    if (isPossible) {
-      sum += gameID;
-    }
+    const power = maxCubes.red * maxCubes.green * maxCubes.blue;
+    totalPowr += power;
   });
 
-  return sum;
+  return totalPowr;
 }
 
-const allCubes = { red: 12, green: 13, blue: 14 };
-const result = idTotals(games, allCubes);
+const result = calcMinCubesAndTotalPower(games);
 console.log(result);
